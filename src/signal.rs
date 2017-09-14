22c716f674514ccaf414e530f8d2dfa3dbfdcbad
Takeru Ohta <phjgt308@gmail.com>
2017-09-14 21:06:51 +0900

    Add `signal` module

---
+++ src/signal.rs
@@ -0,0 +1,19 @@
+use std::io::Write;
+
+use Result;
+use io::SliceReader;
+use literal;
+
+#[derive(Debug)]
+pub struct DynamicTableSizeUpdate {
+    pub max_size: u16,
+}
+impl DynamicTableSizeUpdate {
+    pub fn encode<W: Write>(&self, writer: W) -> Result<()> {
+        track!(literal::encode_u16(writer, 0b001, 5, self.max_size))
+    }
+    pub fn decode(reader: &mut SliceReader) -> Result<Self> {
+        let max_size = track!(literal::decode_u16(reader, 5))?.1;
+        Ok(DynamicTableSizeUpdate { max_size })
+    }
+}
